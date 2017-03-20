32414226c994197ffbb250636d174f2c3a2ea1db
TheSuese <dan@danmckinnon.net>
2017-03-20 14:30:49 -0700

    Creating dev-ops/live-ops settings pattern

---
+++ editor/js/SettingsLive.js
@@ -0,0 +1,15 @@
+//Maintain seperate versions of this file for different develop and live branches
+//~ $.extend(Settings,{
+	//live:true,
+	//~ github:{
+		//~ appName:"goose",
+		//~ clientID:"1354aa390b07f1d30c92"
+	//~ }
+//~ })
+$.extend(Settings,{
+	life:false
+	github:{
+		appName:"goose_dev",
+		clientID:"dacbd1afa13e853aa2dd"
+	}
+})
