34059ea1d6af2a39fbb32635e221571968ff7b42
TheSuese <dan@danmckinnon.net>
2017-03-20 14:50:56 -0700

    Settings are different for each branch

--- editor/js/SettingsLive.js
+++ editor/js/SettingsLive.js
@@ -1,15 +1,8 @@
 //Maintain seperate versions of this file for different develop and live branches
-//~ $.extend(Settings,{
-	//live:true,
-	//~ github:{
-		//~ appName:"goose",
-		//~ clientID:"1354aa390b07f1d30c92"
-	//~ }
-//~ })
 Settings = $.extend(true,Settings,{
 	live:true,
 	github:{
-		appName:"goose_dev",
-		clientID:"dacbd1afa13e853aa2dd"
+		appName:"goose",
+		clientID:"1354aa390b07f1d30c92"
 	}
 })
