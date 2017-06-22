582ecfef0382bcffeb857a3d338939e32f91a30b
TheSuese <dan@danmckinnon.net>
2017-06-21 17:16:32 -0700

    Settings live

--- editor/js/SettingsLive.js
+++ editor/js/SettingsLive.js
@@ -1,11 +1,11 @@
-//~ //Maintain seperate versions of this file for different develop and live branches
-//~ Settings = $.extend(true,Settings,{
-	//~ live:true,
-	//~ github:{
-		//~ appName:"goose",
-		//~ clientID:"28e81d5883f3f4e27e46",
-		//~ login:"hololabs",
-		//~ repo:"wollstonecraft.com",
-		//~ branch:"gh-pages"
-	//~ }
-//~ })  
+//Maintain seperate versions of this file for different develop and live branches
+Settings = $.extend(true,Settings,{
+	live:true,
+	github:{
+		appName:"goose",
+		clientID:"28e81d5883f3f4e27e46",
+		login:"hololabs",
+		repo:"wollstonecraft.com",
+		branch:"gh-pages"
+	}
+})  
