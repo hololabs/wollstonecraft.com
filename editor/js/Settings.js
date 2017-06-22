a1eb9f36ddf35a816e8064a2cf9cfe3e3e90b57e
TheSuese <dan@danmckinnon.net>
2017-06-21 17:20:44 -0700

    Added migration to new graph version

--- editor/js/Settings.js
+++ editor/js/Settings.js
@@ -6,7 +6,7 @@ var Settings = {
 	// -- OUTPUT -- //
 	niceSaveFormat:true,		// Save JSON Files in human-readable format	
 	//saveFormat:"goose2017-05-30-1707",
-	saveFormat:"goose2017-06-21-1657",
+	saveFormat:"goose2017-06-21-1720",
 	
 	// -- GITHUB -- //	
 	github:{
