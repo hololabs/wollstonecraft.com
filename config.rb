96cce46e49e704ccb687fa4cd09ace65b7912b62
TheSuese <dan@danmckinnon.net>
2017-07-05 15:24:45 -0700

    Added navigation

--- config.rb
+++ config.rb
@@ -1,12 +1,13 @@
 # Activate and configure extensions
 # https://middlemanapp.com/advanced/configuration/#configuring-extensions
 
-activate :livereload
 
 activate :autoprefixer do |prefix|
   prefix.browsers = "last 2 versions"
 end
 
+activate :livereload
+set :haml, { :format => :html5 }
 # Layouts
 # https://middlemanapp.com/basics/layouts/
 
@@ -14,6 +15,41 @@ end
 page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
+
+
+helpers do
+	
+	def globals()
+		if !defined?@glob then
+			@glob = YAML.load_file('source/global.yaml')
+		end
+		return @glob
+	end
+
+	def hyphenate(title)
+		return title.gsub(/\s+/, '-').downcase
+	end
+	
+	def nav(menu=globals["navigation"], type="header", htmlclass="main" )
+		
+		html = ""
+		html += "<nav class=\""+htmlclass+"\">"
+		menu.each do |item|
+			if item.has_key?(type) then
+				html += link_to(item["title"],item["page"], :"data-subnav"=>hyphenate(item["title"]))
+			end
+		end
+		html += "</nav>"
+		
+		menu.each do |item|
+			if item.has_key?(type) && item.has_key?("subnav") then
+				html += nav(item["subnav"], type, "submenu " + hyphenate(item["title"] ))
+			end
+		end
+		return html
+	end
+	
+end
 
 # With alternative layout
 # page '/path/to/file.html', layout: 'other_layout'
