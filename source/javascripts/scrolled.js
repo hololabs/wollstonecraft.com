7c6cc7b499ff4b6446640988e71f7e725272b437
TheSuese <dan@danmckinnon.net>
2017-07-07 08:43:48 -0700

    Fixed an issues where menu would bounce if exactly at scroll threshold

--- source/javascripts/scrolled.js
+++ source/javascripts/scrolled.js
@@ -1,18 +1,21 @@
 //= require "jquery-3.2.1.min.js"
+//= require "resize_header_spacing.js"
 
+//Warning: This is hacky
 var suppress_scroll = false;
 (function(){
 	var scroll_duration = 0.3;
 	var old_scroll_active = false
-	var scroll_distance = 1.5;		//in em
-	var scroll_threshold = 0.2;
+	var scroll_distance = 5;		//in em
 	$(document).on("scroll",function(){
 		if ( suppress_scroll ){
 			return;
 		}
-		//var font_size = parseInt($("body").css("font-size"))	
+		var font_size = parseInt($("body").css("font-size"))	
 		var scroll = $(document).scrollTop() 
-		var scroll_in_em = scroll / 100;
+		var scroll_in_em = scroll / font_size;
+		var spacer_height_in_em = $("div#header-spacer").height() / font_size
+		scroll_in_em -= spacer_height_in_em
 		
 		var scroll_active = scroll_in_em > scroll_distance
 		if ( scroll_active != old_scroll_active ){
