50adee7fed972e9f7c6077134a9cbc23a2d6db25
TheSuese <dan@danmckinnon.net>
2017-07-28 21:44:08 -0700

    Added loading screen to the game

--- source/javascripts/the-game.js
+++ source/javascripts/the-game.js
@@ -1,31 +1,65 @@
-(function(){
-	var doneLoading = false;
-	function UnityProgress(e,t){
-		var progress = Math.floor(t*1000)/10
-		$("div#progress > div.amount").css( "width", progress + "%" )
-		if ( t == 1 && !doneLoading){
-			doneLoading = true
-			$("div.webgl-content").css("visibility","visible")
-			$("div#progress").css("display","none")
+$(document).ready(function(){
+	
+	$("div.bleh-game").each(function(){
+		var target = $(this)
+		var game_container = document.createElement("div")
+		var loading_screen = document.createElement("div")
+		var gear1 = document.createElement("div")
+		var gear2 = document.createElement("div")
+		var gear3 = document.createElement("div")
+		var w = document.createElement("div")
+		var caption = document.createElement("div")
+		
+		var loading = true
+		
+		$(loading_screen).append(gear1)
+		$(loading_screen).append(gear2)
+		$(loading_screen).append(gear3)
+		$(loading_screen).append(w)
+		$(loading_screen).append(caption)
+
+		$(loading_screen).addClass("loading_screen")
+		$(gear1).addClass("gear1")
+		$(gear2).addClass("gear2")
+		$(gear3).addClass("gear3")		
+		$(w).addClass("w")
+		$(caption).addClass("caption")
+		$(caption).html("Loading...")
+		
+		$(this).append(loading_screen)
+		$(this).append(game_container)
+
+		
+		function UnityProgress(e,t){
+			if (t >= 1 ){
+				$(loading_screen).addClass("loaded")
+			}
 		}
+		
+		function DoResize(){
+			var width = $(game_container).width()
+			var height = (width / 16) * 9
+			
+			var canvas = $("canvas",game_container).get(0)
+			canvas.width = width
+			canvas.height = height		
+			$(loading_screen).css({
+				"width":width+"px",
+				"height":height+"px"
+			})
+		}
+
+		
+	
+		
+		$(window).on("resize",DoResize)
+		
+		
+		
+		UnityLoader.instantiate(game_container, "Build/Build_WebGL.json", {
+			onProgress: UnityProgress
+		})
 		DoResize()
-	}
-	var gameInstance = UnityLoader.instantiate("gameContainer", "Build/Build_WebGL.json", {onProgress: UnityProgress});
 
-	function DoResize(){
-		//resize the game
-		
-		
-		var width = $("div.webgl-content").width()
-		var height = (width / 16) * 9
-		
-		var canvas = $("div#gameContainer > canvas").get(0)
-		canvas.width = width
-		canvas.height = height
-		
-		
-	}
-
-	$(document).on("ready",DoResize)
-	$(window).on("resize",DoResize)
-})()
+	})
+})
