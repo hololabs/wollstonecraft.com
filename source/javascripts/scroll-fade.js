3968ed5537ce46802bf65da8f4a2c262e662aec1
TheSuese <dan@danmckinnon.net>
2017-10-17 12:31:38 -0700

    Hid/reduced some content,resize index banner based on width, fixed characters on banner floating,  added background and bottom methods to paralax, added some smoothing to scrolling

--- source/javascripts/scroll-fade.js
+++ source/javascripts/scroll-fade.js
@@ -12,117 +12,6 @@ Math.lerp = function (value1, value2, amount) {
 	return value1 + (value2 - value1) * amount;
 };
 
-//~ jQuery.fn.extend({
-	//~ scrollFade:function(optionsIn){
-		
-		//~ var options = $.extend( {
-			//~ min_opacity:1,
-			//~ max_opacity:0,
-			
-			//~ scroll_start:0,
-			//~ scroll_end:10,
-			
-			//~ min_top:0,
-			//~ max_top:0,
-			
-			//~ smoothing:0.1,
-			//~ scroll_relative:"none"
-			
-		//~ },optionsIn)
-		
-		//~ switch(options.scroll_relative){
-			//~ case "top":
-			//~ break;
-			//~ case "bottom":
-			//~ break;
-		//~ }
-		//~ var opacity = options.min_opacity
-		//~ var target_opacity = options.min_opacity
-		//~ var target_top = 0
-		//~ var top = 0
-		//~ var target = $(this)
-		//~ var cssObject = {}
-		//~ var doTop = options.max_top != options.min_top
-		//~ var doOpacity = options.max_opacity != options.min_opacity
-		
-		//~ var running = false
-		//~ function Update(){
-			//~ opacity = Math.lerp( opacity, target_opacity, options.smoothing )
-			//~ top = Math.lerp( top, target_top, options.smoothing)
-			
-			
-			
-			//~ if ( doTop ){
-				//~ cssObject.top = top + "em"
-			//~ }
-			//~ if ( doOpacity ){
-				//~ cssObject.opacity = opacity
-			//~ }
-			//~ target.css(cssObject)
-			
-			//~ if ( Math.abs(target_opacity - opacity) <= 0.01 ){
-				//~ running = false
-				//~ return
-			//~ }
-			
-			//~ requestAnimationFrame(Update)
-			
-		//~ }
-		
-		//~ function SetTarget( o, t ){
-			//~ target_opacity = o
-			//~ target_top = t
-			//~ if ( running == false ){
-				//~ running = true
-				//~ interval = requestAnimationFrame(Update)
-			//~ }
-		//~ }
-		//~ function UpdateScroll(){
-			//~ var font_size = parseInt($("body").css("font-size"))	
-			//~ var scroll = $(document).scrollTop() 
-			//~ var scroll_in_em = scroll / font_size;			
-						
-			//~ var a = (scroll_in_em - options.scroll_start) / (options.scroll_end-options.scroll_start);
-			
-			//~ SetTarget(
-				//~ Math.lerp( options.min_opacity, options.max_opacity, a  ),
-				//~ Math.lerp( options.min_top, options.max_top, a  ),
-			//~ )
-			
-		//~ }
-		//~ $(document).on("scroll",UpdateScroll)
-		//~ UpdateScroll()			
-		
-	//~ }
-//~ })
-
-//~ $(document).ready(function(){
-	//~ function parseFloatFromAttr( target, attr, default_value ){		
-		//~ var n = parseFloat(target.attr(attr))
-		//~ return !isNaN(n) ? n : default_value
-	//~ }
-	//~ $("*[data-scroll-fade]").each(function(){
-		//~ var options = {
-			//~ min_opacity: parseFloatFromAttr($(this),"data-min-opacity",1),
-			//~ max_opacity: parseFloatFromAttr($(this),"data-max-opacity",0),
-			//~ scroll_start: parseFloatFromAttr($(this),"data-scroll-start",0),
-			//~ scroll_end: parseFloatFromAttr($(this),"data-scroll-end",10),
-			//~ min_top: parseFloatFromAttr($(this),"data-min-top",0),
-			//~ max_top: parseFloatFromAttr($(this),"data-max-top",0),
-			//~ smoothing: parseFloatFromAttr($(this),"data-smoothing",0.1),
-			//~ relative: $(this).attr("data-scroll-relative")
-		//~ }
-
-		//~ $(this).scrollFade(options)
-	//~ })
-//~ })
-
-
-
-
-
-
-
 $(document).ready(function(){	
 	
 	var rotation_builder = ["rotate(",0,"deg)"]
@@ -150,12 +39,25 @@ $(document).ready(function(){
 			default_options:{
 				speed:0.5,
 				offset:0,
-				relative:"absolute"
+				relative:"absolute",
+				mode:"top"
 			},
 			animate:function( target, options, scroll ){
-				
-				var y =(-1 * options.speed * scroll) + options.offset
-				target.css( {top: y + "em" })
+				var y;
+				switch( options.mode ){
+					case "background-bottom":
+						y =(1 * options.speed * scroll) + options.offset
+						target.css("background-position","center " + y + "%");
+					break;
+					case "bottom":
+						y =(1 * options.speed * scroll) + options.offset
+						target.css( {bottom: y + "em" })
+					break;
+					case "top":
+						y =(-1 * options.speed * scroll) + options.offset
+						target.css( {top: y + "em" })
+					break;
+				}
 			}
 		},
 		
@@ -273,10 +175,20 @@ $(document).ready(function(){
 	
 	var end_time = 0
 	var start_time = 0 
+	var last_t = 0;
+	var t
+	var scroll = 0;
+	var scroll_dest = 0;
+	var smoothing = 25;
 	function Animate(){
-		var t = Date.now()/1000
+		last_t = t;
+		t = Date.now()/1000
+		var delta_t = t - last_t;
 		
-		scroll = ScrollTop()
+		scroll_dest = ScrollTop()
+		
+		scroll = Math.lerp( scroll,scroll_dest,delta_t * smoothing )
+		
 		for( var listener_id in listeners ){
 			var listener = listeners[listener_id]
 			var options = listener.options
@@ -339,7 +251,7 @@ $(document).ready(function(){
 		var new_scroll_destination = ScrollTop()
 		
 		start_time = t
-		end_time = t + length
+		end_time = t + 1
 		
 		if (!running && Math.abs( scroll_destination - new_scroll_destination) >= scroll_threshold ){
 			running = true
